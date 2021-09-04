9b2b882ad1a1534df760d355e1b9548f0d79bb10
Alson33 <garbuja12alson@gmial.com>
2021-09-04 14:12:25 +0545

    add basic index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDom.render(
+  <App />
+  , document.getElementById('root')
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Export default --> we can name it as we want when importing
+// Export const --> we have to name the import same as export
+// We can rename some imports (import Hello as hi from './Hello')
+// if the file is from 3rd party or dependencies we dont have to
+// specify path otherwise we have to
