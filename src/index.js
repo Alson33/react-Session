b3ab61b253927bb9d66b1b470f050ac53a7dbd43
Alson33 <garbuja12alson@gmial.com>
2021-09-05 14:17:54 +0545

    add props and spread operator

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+// import App2 from './App2'
 import App from './App'
 
+// const data = {
+//   age: 20,
+//   address: "bagar",
+// }
+
+// const someArr = ['foo', 'bar']
+
 ReactDom.render(
-  <App />
+  // when sending props we have to include "" or ''
+  // except when they are js objects we use {}
+  // <App2 
+  //   name="Alson" 
+  //   data={data} 
+  //   meta={ {desc:"Hello world"} } 
+  //   arr={someArr}
+  // />
+  // spread operator ... => sends the object by destructuring
+  // <App 
+  //   name="Alson"  
+  //   meta={ {desc:"Hello world"} } 
+  //   arr={someArr}
+  //   {...data}
+  // />
+  <App name="Alson" />
   , document.getElementById('root')
 )
 
